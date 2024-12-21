5a6062769bc4d0e9728847149335e2c1b3c79d73
linsglf <gabriellinsglf@gmail.com>
2024-12-21 16:50:55 -0300

    Estrutura inicial do projeto

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { KeyModule } from './key/key.module';
+import { TransactionModule } from './transaction/transaction.module';
+import { BalanceModule } from './balance/balance.module';
 
 @Module({
-  imports: [],
+  imports: [UserModule, KeyModule, TransactionModule, BalanceModule],
   controllers: [AppController],
   providers: [AppService],
 })
